d07d1699fdef823d3930376c4b319dc8c8204547
Or Roob <orroob28@gmail.com>
2021-03-23 18:27:22 +0200

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -35,46 +35,38 @@ int main(int argc, char* argv[])
 		printf("Error at WSAStartup()\n");
 
 //create socket
-	SOCKET client_s;
+	SOCKET channel_s;
 
-	if ((client_s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+	if ((channel_s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
 		printf("Could not create socket : %d", WSAGetLastError());
 	}
 
 	printf("Socket created.\n");
 
-// Filling server information 
+// Filling server (channel) information 
 	struct sockaddr_in client_addr;
 	client_addr.sin_family = AF_INET;
 	client_addr.sin_port = htons(atoi(port));
 	client_addr.sin_addr.s_addr = inet_addr(IP);//INADDR_ANY;
 
-//connect to the channel
-	//int status = connect(client_s, (SOCKADDR*)&client_addr, sizeof(client_addr));
-
 
 	char chr[12] = { 0 }, coded_chr[16] = { 0 }, buffer[BUFF_SIZE] = { 0 };
 	int pckt_size = 0;
-	//buffer = calloc(BUFF_SIZE, sizeof(char*));
-	if (buffer == NULL)
-	{
-		printf("Memory allocation failed\n");
-	}
-
 	int i = 0;
+
 	while (fread(chr, 1, 11, f))
 	{
 		Encoder(chr, coded_chr);
 		strncpy(buffer + pckt_size, coded_chr, 15);
 		pckt_size += 15;
-		//printf("%d\n", strlen(buffer));
+
 		if (pckt_size == BUFF_SIZE-1)
-		{
+		{//packet is ready
 			printf("sending packet num %d\n", i);
 			i++;
 			//send data
-			if (sendto(client_s, buffer, strlen(buffer), 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
+			if (sendto(channel_s, buffer, strlen(buffer), 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
 			{
 				printf("sendto() failed with error code : %d", WSAGetLastError());
 				exit(EXIT_FAILURE);
@@ -86,21 +78,20 @@ int main(int argc, char* argv[])
 			}
 			pckt_size = 0;
 		}
-		//printf("%d %s\n", (char*)coded_chr, buffer);
-		
 	}
+
 	if (pckt_size != BUFF_SIZE-1)
 	{
 		buffer[pckt_size] = '\0';
 		//send data
-		if (sendto(client_s, buffer, strlen(buffer), 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
+		if (sendto(channel_s, buffer, strlen(buffer), 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
 		}
 		printf("sending packet num %d\n", i);
 	}
-	closesocket(client_s);
+	closesocket(channel_s);
 	fclose(f);
 
 	return 0;
