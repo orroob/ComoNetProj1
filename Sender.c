1c5ca540db9feffa849cae1858fd40996ef63cd5
Or Roob <orroob28@gmail.com>
2021-03-25 17:56:34 +0200

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
 	argv[2] - channel port
 	argv[3] - input file name
 */
-
+	int last_pckt;
 	FILE* f = NULL;
 	f = fopen(argv[3], "rb");
 	if (f == NULL)
@@ -53,15 +53,16 @@ int main(int argc, char* argv[])
 	// Setup timeval variable
 	struct timeval timeout;
 	struct fd_set fds;
-	timeout.tv_sec = 1;
-	timeout.tv_usec = 0;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0.5;
 
-	char chr[12] = { 0 }, coded_chr[16] = { 0 }, buffer[BUFF_SIZE] = { 0 };
+	unsigned char chr[12] = { 0 }, coded_chr[16] = { 0 }, buffer[BUFF_SIZE] = { 0 };
 	int  retval, pckt_size = 0, buffer_len;
+	int flag = 0;
 
 	while (fread(chr, 1, 11, f))
 	{
-/*
+
 		// Check if Ack has been sent from the channel. if so, close the connection, the file and finish execution:
 		//set descriptors
 		FD_ZERO(&fds);
@@ -73,11 +74,6 @@ int main(int argc, char* argv[])
 		{//error occured
 
 			// check errno/WSAGetLastError(), call perror(), etc ...
-		}
-
-		if (retval == 0)
-		{//timeout occured
-			continue;
 		}
 
 		if (retval > 0)
@@ -99,9 +95,14 @@ int main(int argc, char* argv[])
 
 				//close intput file
 				fclose(f);
+
+				flag = 1;
+
+				//finish execution
+				break;
 			}
 		}
-*/
+
 		//encode message and insert it to the packet
 		Encoder(chr, coded_chr);
 		strncpy(buffer + pckt_size, coded_chr, 15);
@@ -110,9 +111,9 @@ int main(int argc, char* argv[])
 		if (pckt_size == BUFF_SIZE-2)
 		{//packet is ready to be sent
 
-
+			Sleep(10);
 			//send data to channel
-			if (sendto(channel_s, buffer, BUFF_SIZE, 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
+			if (sendto(channel_s, buffer, pckt_size, 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
 			{
 				printf("sendto() failed with error code : %d", WSAGetLastError());
 				exit(EXIT_FAILURE);
@@ -127,18 +128,64 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	if (pckt_size != BUFF_SIZE-1)
+	if (pckt_size != BUFF_SIZE-2)
 	{
+		last_pckt = pckt_size;
 		while (pckt_size!= BUFF_SIZE - 1)
 		{
 			buffer[pckt_size] = '\0';
 			pckt_size++;
 		}
 		//send data
-		if (sendto(channel_s, buffer, strlen(buffer), 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
+		if (sendto(channel_s, buffer, last_pckt, 0, (struct sockaddr *) &client_addr, sizeof(client_addr)) == SOCKET_ERROR)
 		{
 			printf("sendto() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (!flag)
+	{
+		while (1)
+		{// Check if Ack has been sent from the channel. if so, close the connection, the file and finish execution:
+		//set descriptors
+			FD_ZERO(&fds);
+			FD_SET(channel_s, &fds);
+
+			//wait for data to be sent by the channel
+			retval = select(channel_s + 1, &fds, NULL, NULL, &timeout);
+			if (retval < 0)
+			{//error occured
+
+				// check errno/WSAGetLastError(), call perror(), etc ...
+			}
+
+			if (retval > 0)
+			{
+				if (FD_ISSET(channel_s, &fds))
+				{//data was received from the channel
+				//we will process it (decode) and write it to the output file
+
+					printf("Sender requesting to connect...\n");
+
+					//Receive data from the channel
+					buffer_len = recvfrom(channel_s, buffer, BUFF_SIZE, 0, (struct sockaddr*)&client_addr, &client_addr_len);
+					//======(we receive the message from the address to which we sent our data to - check for errors)=======================
+
+					//===present the data from Ack before finishing execution =======
+
+					//close connection with the channel
+					closesocket(channel_s);
+
+					//close intput file
+					fclose(f);
+
+					flag = 1;
+
+					//finish execution
+					break;
+				}
+			}
 		}
 	}
 	return 0;
